fd1f029b6e98489b91f7c03fe7274c93165106aa
ashlesh9999 <90056040+ashlesh9999@users.noreply.github.com>
2021-09-03 14:49:12 -0500

    Update ObserverPattern.java

--- ObserverPattern.java
+++ ObserverPattern.java
@@ -1,14 +1,65 @@
-public class ObserverPatternDemo {
-   public static void main(String[] args) {
-      Subject subject = new Subject();
+import java.util.ArrayList;
 
-      new HexaObserver(subject);
-      new OctalObserver(subject);
-      new BinaryObserver(subject);
-
-      System.out.println("First state change: 15");	
-      subject.setState(15);
-      System.out.println("Second state change: 10");	
-      subject.setState(10);
+ abstract class Course {
+    
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    
+    public void attach(Observer observer) {
+       observers.add(observer);
+    }
+    public void detach(Observer observer) {
+       observers.remove(observer);
+    }
+    public void notifyObservers() {
+       for (Observer o : observers)    
+          o.update();
    }
-}
+ }
+ 
+ class ConcreteCourse extends Course {
+  
+  private String courseState;
+  
+  public String getCourseState() {
+    return courseState;
+  }
+  public void setCourseState(String value) {
+    courseState = value;
+  }
+ }
+ 
+ abstract class Observer {
+   
+   abstract public void update();
+ }
+  class ConcreteObserver extends Observer {
+  
+  private String name;
+  private String observerState;
+  private ConcreteCourse course;
+ 
+  public ConcreteObserver(ConcreteCourse course, String name) {
+     this.course = course;
+     this.name = name;
+     
+  }
+  
+  public void update() {
+     observerState = course.getCourseState();
+     System.out.printf("Observer %s's new state is %s\n", name, observerState);
+  }
+ }
+ 
+ public class Client {
+   public static void main(String[] args) {
+      
+      ConcreteCourse s = new ConcreteCourse();
+      s.attach(new ConcreteObserver(s, "1"));
+      s.attach(new ConcreteObserver(s, "2"));
+      s.attach(new ConcreteObserver(s, "9"));
+
+     
+      s.setCourseState("Already registered");
+      s.notifyObservers();
+   }
+ }
